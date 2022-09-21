425281931752db36c4fe5e00e428ae04a7c71d46
Вероника Гришина <grishinaveronika00@gmail.com>
2022-09-21 11:44:41 +0300

    FLIRTING WITH THE TEXT

--- TypingAnimation.py
+++ TypingAnimation.py
@@ -1,19 +1,20 @@
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((500, 150))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont(None, 100)
+window = pygame.display.set_mode((500, 150))  # creating the screen window
+clock = pygame.time.Clock()  # ?
+font = pygame.font.SysFont('cambria', 70)  # setting the font of the text
 
-background = pygame.Surface(window.get_size())
+background = pygame.Surface(window.get_size())  # creating the surface
 
-text = 'ALLO'
+text = 'HELLO THERE'
 text_len = 0
 typewriter_event = pygame.USEREVENT + 1
-pygame.time.set_timer(typewriter_event, 100)
+pygame.time.set_timer(typewriter_event, 170)
 text_surf = None
 
 run = True
+
 while run:
     clock.tick(60)
     for event in pygame.event.get():
@@ -23,9 +24,10 @@ while run:
             text_len += 1
             if text_len > len(text):
                 text_len = 0
-            text_surf = None if text_len == 0 else font.render(text[:text_len], True, (255, 255, 128))
+            text_surf = None if text_len == 0 else font.render(text[:text_len], True, (255, 255, 255))
 
     window.blit(background, (0, 0))
+
     if text_surf:
         window.blit(text_surf, text_surf.get_rect(midleft = window.get_rect().midleft).move(40, 0))
     pygame.display.flip()
