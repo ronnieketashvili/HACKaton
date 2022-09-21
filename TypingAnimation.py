3d3111330adc6ba1f46fea706960f81bc6aceefd
Вероника Гришина <grishinaveronika00@gmail.com>
2022-09-21 14:37:35 +0300

    working on texts

--- TypingAnimation.py
+++ TypingAnimation.py
@@ -1,6 +1,97 @@
 import pygame
 import Questions
 
+
+def print_text():
+    run = True
+    row = 0
+    text_surf = None
+    text_len = 0
+    typewriter_event = pygame.USEREVENT + 1
+    pygame.time.set_timer(typewriter_event, 20)
+
+    for questions in questions_tuples:
+        if len(questions) != 0:
+            question_lines = questions[0].splitlines()
+            answer_a = questions[1].splitlines()
+            answer_b = questions[2].splitlines()
+            input = ''
+            row = 0
+
+            for line in question_lines:
+                text = line
+                row += 20
+                run = True
+
+                while run:
+                    clock.tick(60)
+                    for event in pygame.event.get():
+                        if event.type == typewriter_event:
+                            text_len += 1
+                            if text_len > len(text):
+                                text_len = 0
+                                run = False
+                            text_surf = None if text_len == 0 else font.render(text[:text_len], True, (255, 255, 255))
+
+                    if text_surf:
+                        window.blit(text_surf, (0, row))
+                    pygame.display.flip()
+
+            while input == '' and len(question_lines) != 0:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_a:
+                            input = 'a'
+                            print_answer(answer_a)
+                            break
+                        elif event.key == pygame.K_b:
+                            input = 'b'
+                            print_answer(answer_b)
+                            break
+
+            window.blit(background, (0, 0))
+            print(input)
+
+    pygame.quit()
+
+
+def print_answer(answer):
+    window.blit(background, (0, 0))
+    run = True
+    row = 0
+    text_surf = None
+    text_len = 0
+    typewriter_event = pygame.USEREVENT + 1
+    pygame.time.set_timer(typewriter_event, 20)
+
+    for line in answer:
+        text = line
+        row += 20
+        run = True
+
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == typewriter_event:
+                    text_len += 1
+                    if text_len > len(text):
+                        text_len = 0
+                        run = False
+                    text_surf = None if text_len == 0 else font.render(text[:text_len], True, (255, 255, 255))
+
+            if text_surf:
+                window.blit(text_surf, (0, row))
+            pygame.display.flip()
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                return
+
+
 pygame.init()
 window = pygame.display.set_mode((700, 700))  # creating the screen window
 clock = pygame.time.Clock()  # ?
@@ -8,51 +99,6 @@ font = pygame.font.SysFont('microsoftyibaiti', 20)  # setting the font of the te
 
 background = pygame.Surface(window.get_size())  # creating the surface
 
-text = Questions.question_1()
-lines = text.splitlines()
+questions_tuples = Questions.question_list
 
-text_len = 0
-typewriter_event = pygame.USEREVENT + 1
-pygame.time.set_timer(typewriter_event, 50)
-text_surf = None
-
-run = True
-row = 0
-
-for line in lines:
-
-    text = line
-    row += 20
-    run = True
-
-    while run:
-        clock.tick(60)
-        for event in pygame.event.get():
-            # if event.type == pygame.QUIT:
-            #     run = False
-            if event.type == typewriter_event:
-                text_len += 1
-                if text_len > len(text):
-                    text_len = 0
-                    run = False
-                text_surf = None if text_len == 0 else font.render(text[:text_len], True, (255, 255, 255))
-
-        # window.blit(background, (0, 0))
-
-        if text_surf:
-            window.blit(text_surf, (0, row))
-        pygame.display.flip()
-
-input = ''
-while input == '':
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                input = 'a'
-            elif event.key == pygame.K_b:
-                input = 'b'
-
-print(input)
-pygame.quit()
+print_text()
